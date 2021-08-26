4eccb2948069d1d58f044bfaea7f9fa35613c0ef
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-08-25 22:00:23 -0400

    Created variable for delinquency levels, added histograms, tried plot, jotted down notes on desired variables

--- HOPE_loans_delinquency_analysis.R
+++ HOPE_loans_delinquency_analysis.R
@@ -32,19 +32,42 @@ summary(loans$CustomerId)
 
 #inspect distribution of the variables
 # histogram
+##histogram of Disbursement Amount:
+hist(loans$DisbursedAmount, 200, main="Disbursement amount", xlab="DisbursementAmount", ylab="Number of loans with that amount")
+##histogram of LateInstallments:
+hist(loans$LateInstallments, 30, main="LateInstallments", xlab="DisbursementAmount", ylab="Number of loans")
+##
+hist(loans$WorkDaysLoanWasInArrears, 300, main="WorkDaysLoanWasInArrears", xlab="WorkDaysLoanWasInArrears", ylab="Number of loans")
+
+###Check X label !
+
 #inspect outliers for each variable:
 #boxplot
 
+##plot data
+plot(loans$LateInstallments, factor(loans$MaritalStatus), type="p", xlim=range(60,80), ylim=range(70,320), main="Relationship Between MaritalStatus and LateInstallments", xlab="LateInstallments", ylab="MaritalStatus")
 
 
 #Step 3: Creating new variables
 # Low, medium and high delinquency:
 
+#Criteria:
+#"low delinquency" as having 0-2 late installments; 
+#"medium delinquency" as having 3-4 late installments; 
+#and "high delinquency" as having 4+ late installments per loan (not per customer).
+Delinquency <- factor(loans$LateInstallments)
+levels(Delinquency) <- list(Low = 0:2, Medium = 3:4, High = 5:18 )
+summary(factor(Delinquency))
+
 # Make date variable : day, month, year
 
 #Age groups:
 
-#
+#Additional variables that would be relevant if available:
+#occupation
+#the use of the loan (ex: agriculture, shop, sewing business, etc)
+#number of people in household
+#location (country, city, )
 
 #Ideas: 
 
