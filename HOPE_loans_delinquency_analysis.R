bc4ccff80b7f121fa71a83418a29699a62b2111f
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-08-27 17:25:53 -0400

    updates on ideas; added boxplots and tables

--- HOPE_loans_delinquency_analysis.R
+++ HOPE_loans_delinquency_analysis.R
@@ -31,15 +31,15 @@ summary(loans$CustomerId)
 #Check: unique LoanID and unique Customer ? No. See distribution of loans per customer
 #See histogram
 hist(loans$CustomerId, 65421, main="CustomerID", xlab="CustomerID", ylab="Number of loans per customer")
+
 #Count how many customers have a certain number of loans
 # insight: create variable to how many loans each customer has had and include that in analysis
 # However: be careful since the  individual FE may capture that ? check
+
 #install.packages("plyr") # if not installed previously
 library(plyr)
 count(loans, c("loans$CustomerID", "loans$LoanID"))
 # Maybe: some info about difference of date between disbursements ? (create variable)
-
-#Panel Data
 
 #inspect missing data:
 summary(loans)
@@ -52,18 +52,36 @@ summary(loans_clean)
 
 
 #inspect distribution of the variables
-# histogram
+# Histograms:
+##histogram of LateInstallments:
+hist(loans$LateInstallments, main="Distribution of Number of Late Installments per Loan", xlab="Number of late installments per loan", ylab="Number of loans")
 ##histogram of Disbursement Amount:
 hist(loans$DisbursedAmount, 200, main="Disbursement amount", xlab="DisbursementAmount", ylab="Number of loans with that amount")
-##histogram of LateInstallments:
-hist(loans$LateInstallments, 30, main="LateInstallments", xlab="DisbursementAmount", ylab="Number of loans")
-##
-hist(loans$WorkDaysLoanWasInArrears, 300, main="WorkDaysLoanWasInArrears", xlab="WorkDaysLoanWasInArrears", ylab="Number of loans")
+## lots of zeros: tobit is an appropriate model 
+hist(loans$WorkDaysLoanWasInArrears, main="WorkDaysLoanWasInArrears", xlab="WorkDaysLoanWasInArrears", ylab="Number of loans")
 
 ###Check X label !
 
 #inspect outliers for each variable:
-#boxplot
+#Boxplot
+library(ggplot2)
+ggplot(loans,aes(x=ProductGroup1Name,y=LateInstallments,
+               fill=factor(ProductGroup1Name)))+
+  geom_boxplot()+
+  geom_jitter()+
+  labs(title="Boxplot for Late Installments per ProductGroup1Name",x="ProductGroup1Name",y="Late Installments")+
+  theme_bw()+coord_flip()+
+  theme(legend.position = "none")
+
+library(ggplot2)
+ggplot(loans,aes(x=MaritalStatus,y=LateInstallments,
+                 fill=factor(MaritalStatus)))+
+  geom_boxplot()+
+  geom_jitter()+
+  labs(title="Boxplot for Late Installments per MaritalStatus",x="MaritalStatus",y="Late Installments")+
+  theme_bw()+coord_flip()+
+  theme(legend.position = "none")
+
 
 ##plot data
 plot(loans$LateInstallments, factor(loans$MaritalStatus), type="p", xlim=range(60,80), ylim=range(70,320), main="Relationship Between MaritalStatus and LateInstallments", xlab="LateInstallments", ylab="MaritalStatus")
@@ -76,23 +94,37 @@ plot(loans$LateInstallments, factor(loans$MaritalStatus), type="p", xlim=range(6
 #"low delinquency" as having 0-2 late installments; 
 #"medium delinquency" as having 3-4 late installments; 
 #and "high delinquency" as having 4+ late installments per loan (not per customer).
-Delinquency <- factor(loans$LateInstallments)
-levels(Delinquency) <- list(Low = 0:2, Medium = 3:4, High = 5:18 )
-summary(factor(Delinquency))
-
-#Useful to logit/probit models?
-is_low <- factor(loans$LateInstallments)
-levels(is_low) <- list(Low = 0:2, not_low = 3:18 )
-summary(factor(is_low))
-
-is_high <- factor(loans$LateInstallments)
-levels(is_high) <- list(not_high = 0:4, high = 4:18 )
-summary(factor(is_high))
-
+loans$Delinquency <- factor(loans$LateInstallments)
+levels(loans$Delinquency) <- list(None = 0, Low = 1:2, Medium = 3:4, High = 5:18 )
+#levels(loans$Delinquency) <- list(Low = 0:2, Medium = 3:4, High = 5:18 )
+summary(factor(loans$Delinquency))
 
 # Make date variable : day, month, year #inspect how it plots
 
-#Age groups:
+#Age:
+#Separate year from BirthDate column:
+# method 1:
+loans$Date_of_birth <- data.frame(do.call("rbind", strsplit(as.character(loans$BirthDate), "/", fixed = TRUE)))
+lapply(Date_of_birth, transform, Date_of_birth.X3 = 2021 -  Date_of_birth.X3)
+
+#method 2:
+install.packages("stringr")
+library(stringr)
+as(loans$BirthDate, integer , strict=TRUE, ext)
+str_split_fixed(loans$BirthDate, "/", 3)
+lapply(Date_of_birth, transform, Date_of_birth.X3 = 2021 -  Date_of_birth.X3)
+
+
+#method 3:
+install.packages("tidyr")
+library(tidyr) 
+loans %>%
+separate(loans$BirthDate, c("month", "day", "year"), "/")
+
+library(eeptools)
+x <- as.Date(loans$BirthDate)
+age_calc(x[1],x[2], units = "years")
+# Age groups:
 
 #Additional variables that would be relevant if available:
 #occupation
@@ -100,20 +132,48 @@ summary(factor(is_high))
 #number of people in household
 #location (country, city, )
 
-#Ideas: 
+#Step 4:
 
-#OLS:
-lm1 <- (, data=loans)
+# Tables:
+
+#Deliquency rates per marital status:
+table(loans$MaritalStatus, loans$Delinquency)
+#obs: rename column for typo (If I have time)
+
+#Deliquency rates per Gender:
+table(loans$Gender, loans$Delinquency)
+table(loans$Gender)
+Female_percentage <- 39061 / (39061 + 26313)
+Female_percentage
+Male_percentage <- 26313 / (39061 + 26313)
+Male_percentage
+
+#Deliquency rates per Age:
+
+#Deliquency rates per ProductGroupName:
+
+#Deliquency rates per DisbursementDate:
+
+#Deliquency rates per DisbursedAmount:
+
+
+
+
+# simple OLS: with Late Installments
+# simple regression with time and individual FE 
+lm1 <- lm(loans$LateInstallments ~ factor(loans$Gender) + factor(loans$MaritalStatus) + loans$DisbursedAmount + factor(loans$ProductGroup1Name) + factor(loans$CustomerId) + loans$WorkDaysLoanWasInArrears)
 summary(lm1)
 
-# simple regression with time and individual FE 
 
-# logit and probit models
+# tobit and ologit models:
+
+# tobit : interpretation - signal, but for magnitude, calculate the marginal effect
+
 
 #Machine learning methods
 library(caret)
 
-# Try: random forest
+# Try: random forest, decision tree, 
 #feature of importance
 #ConfusionMatrix
 #CorrelationMatrix
